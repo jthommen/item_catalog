1349e2e7707616c0748da91bb35a17f90212f9eb
Juri Thommen <jthommen@google.com>
2017-02-16 18:05:24 +0000

    Add handlers for menu item crud

--- project.py
+++ project.py
@@ -1,9 +1,6 @@
 # Import flask class from Flask library
 from flask import Flask
 
-# Create Flask instance with running app as arg
-app = Flask(__name__)
-
 ## Imports db and db connection modules
 # Imports db engine initialization
 from sqlalchemy import create_engine
@@ -11,9 +8,14 @@ from sqlalchemy.orm import sessionmaker
 # Imports db setup
 from database_setup import Base, Restaurant, MenuItem
 
+# Create Flask instance with running app as arg
+app = Flask(__name__)
+
 # Specifies and creates db connection
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
+
+# Creates db connection session item
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
@@ -26,11 +28,26 @@ def restaurantmenu(restaurant_id):
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     output = ""
     for item in items:
-        output += item.name + "</br>"
+        output += item.name + str(item.id) +"</br>"
         output += item.price + "</br>"
         output += item.description
         output += "</br></br>"
     return output
+
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    output = ""
+    output += "Here you can add new Menu items!"
+    return output
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menuitem_id>/edit/')
+def editMenuItem(restaurant_id, menuitem_id):
+    return "Page to edit a menu item!"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menuitem_id>/delete/')
+def deleteMenuItem(restaurant_id, menuitem_id):
+    return "Page to delete a menu item"
+
 
 # Script only runs when directly run in python
 # and not imported as module
